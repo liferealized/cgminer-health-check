7512cc976e9fe9a2960f908fdf6a9fb9ca700bf0
James Gibson <me@iamjamesgibson.com>
2013-01-12 10:50:49 -0500

    fixed the if statement

--- healthcheck.sh
+++ healthcheck.sh
@@ -13,8 +13,7 @@ do
   # send the "devs" command to cgminer to see
   # if any device are dead. if so, run
   # the restart command from the config
-  if [[ echo "devs|" | nc localhost "$x" | grep -i "dead" ]];
+  if echo "devs|" | nc localhost "$x" | grep -i "dead";
     then eval "$restartcmd"
   fi
-
 done

8870340f8f168a0bf8c43e0b80e2c8dfcdfb9f71
James Gibson <me@iamjamesgibson.com>
2013-01-12 10:22:22 -0500

    first commit

---
+++ healthcheck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# get our restart command and the ports
+# to talk with cgminer on
+source health.conf
+
+
+# loop through our ports for when we have multiple
+# cgminer instances on this machine
+for x in "${ports[@]}"
+do
+
+  # send the "devs" command to cgminer to see
+  # if any device are dead. if so, run
+  # the restart command from the config
+  if echo "devs|" | nc localhost "$x" | grep -i "dead"; 
+    then $restartcmd
+  fi
+
+done
